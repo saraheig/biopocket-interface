0dfa4355e6d0cf3b5e590e893f5c8cbb2258ab80
saraheig <sarah.composto@heig-vd.ch>
2017-07-27 14:07:20 +0000

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727140518) do
+ActiveRecord::Schema.define(version: 20170727140637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
