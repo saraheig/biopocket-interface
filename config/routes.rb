0fef858b317bbfa8ff766bc244344f1069373cc7
saraheig <sarah.composto@heig-vd.ch>
2017-07-29 13:23:08 +0000

    Modify table view + routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'tables/index'
-
-  resources :works
-  resources :categories
-  resources :topics
+  get '/tables', to: 'tables#index'
+  
+  resources :works, path: '/tables/works'
+  resources :categories, path: '/tables/categories'
+  resources :topics, path: '/tables/topics'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
