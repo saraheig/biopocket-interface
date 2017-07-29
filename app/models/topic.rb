989a8d45e63e7ad6b73cf74d88066b7a0a53f032
saraheig <sarah.composto@heig-vd.ch>
2017-07-29 10:13:41 +0000

    Add topic validations with messages

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,9 @@
 class Topic < ApplicationRecord
-    has_many :works, dependent: :destroy
+  has_many :works, dependent: :destroy
+  
+  validates_presence_of :title, :message => "Le titre du thème doit être spécifié."
+  validates_length_of :title, :maximum => 50, :message => "Le titre du thème doit avoir moins de 50 caractères."
+  validates_uniqueness_of :title, :case_sensitive => false, :message => "Le titre du thème est déjà utilisé."
+  validates_length_of :description, :maximum => 250, :message => "La description du thème doit avoir moins de 250 caractères."
+  
 end
