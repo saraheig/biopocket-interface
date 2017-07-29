1ea95d75e4cb8b06ff33c8c73bca26d24869ec21
saraheig <sarah.composto@heig-vd.ch>
2017-07-29 12:02:23 +0000

    Add category validations with messages

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
-    has_many :works, dependent: :destroy
+  has_many :works, dependent: :destroy
+    
+  validates_presence_of :title, :message => "Le titre du type d'action doit être spécifié."
+  validates_length_of :title, :maximum => 50, :message => "Le titre du type d'action doit avoir moins de 50 caractères."
+  validates_uniqueness_of :title, :case_sensitive => false, :message => "Le titre du type d'action est déjà utilisé."
+  
 end
