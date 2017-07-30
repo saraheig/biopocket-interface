e1b7ca8d53602f02b98de54cd8d5dbfd31d13b38
saraheig <sarah.composto@heig-vd.ch>
2017-07-30 10:01:24 +0000

    Fix category controller test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -17,7 +17,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create category" do
     assert_difference('Category.count') do
-      post categories_url, params: { category: { title: @category.title } }
+      post categories_url, params: { category: { title: 'NewTitle' } }
     end
 
     assert_redirected_to category_url(Category.last)
