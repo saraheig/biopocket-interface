e4f68a0fc9d043295e7a097a95086fcd71b5ea8a
saraheig <sarah.composto@heig-vd.ch>
2017-07-30 07:32:30 +0000

    Add default route to config

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :categories, path: '/tables/categories'
   resources :topics, path: '/tables/topics'
   
+  # Default route (if unknown url)
+  match '*path' => redirect('/tables'), via: [:get, :post]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
