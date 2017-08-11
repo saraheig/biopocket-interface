025ce1c5bb51030938fadbd0f1bdfad1c16fc0cb
Sarah <sarah.composto@heig-vd.ch>
2017-08-11 21:18:55 +0000

    Add User tests and form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password validations: false # Custom validations: see below
+  
+  validates_length_of :firstname, :maximum => 15, :message => "Le prénom doit avoir moins de 15 caractères."
+  validates_length_of :lastname, :maximum => 15, :message => "Le nom de famille doit avoir moins de 15 caractères."
+  validates_presence_of :email, :message => "Un e-mail doit être spécifié."
+  validates_uniqueness_of :email, :case_sensitive => false, :message => "Cet e-mail est déjà utilisé."
+  validates_uniqueness_of :pseudo, :case_sensitive => false, :message => "Ce pseudo est déjà utilisé."
+  validates_length_of :pseudo, :in => 5..15, :message => "Un pseudo doit être spécifié et avoir entre 5 et 15 caractères."
+  validates_length_of :password, :minimum => 5, :message => "Un mot de passe doit être spécifié et avoir plus de 5 caractères."
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+  
+  test 'ensures that firstname length is less than 15' do
+    user = User.new(firstname: 'Lorem ipsum dol.', lastname: @user.lastname, pseudo: 'titolino', email: 'tutu@toto.com', password: @user.password_digest)
+    assert_not user.valid?
+    assert_equal [:firstname], user.errors.keys
+  end
+  
+  test 'ensures that lastname length is less than 15' do
+    user = User.new(firstname: @user.firstname, lastname: 'Lorem ipsum dol.', pseudo: 'titolino', email: 'tutu@toto.com', password: @user.password_digest)
+    assert_not user.valid?
+    assert_equal [:lastname], user.errors.keys
+  end
+  
+  test 'ensures that email exists' do
+    user = User.new(firstname: @user.firstname, lastname: @user.lastname, pseudo: 'titolino', email: nil, password: @user.password_digest)
+    assert_not user.valid?
+    assert_equal [:email], user.errors.keys
+  end
+  
+  test 'ensures that email is unique' do
+    user = User.new(firstname: @user.firstname, lastname: @user.lastname, pseudo: 'titolino', email: @user.email, password: @user.password_digest)
+    assert_not user.valid?
+    assert_equal [:email], user.errors.keys
+  end
+  
+  test 'ensures that pseudo is unique' do
+    user = User.new(firstname: @user.firstname, lastname: @user.lastname, pseudo: @user.pseudo, email: 'tutu@toto.com', password: @user.password_digest)
+    assert_not user.valid?
+    assert_equal [:pseudo], user.errors.keys
+  end
+  
+  test 'ensures that the pseudo length is minimumn 5' do
+    user = User.new(firstname: @user.firstname, lastname: @user.lastname, pseudo: '1234', email: 'tutu@toto.com', password: @user.password_digest)
+    assert_not user.valid?
+    assert_equal [:pseudo], user.errors.keys
+  end
+  
+  test 'ensures that the pseudo length is maximum 15' do
+    user = User.new(firstname: @user.firstname, lastname: @user.lastname, pseudo: '12345678910123456', email: 'tutu@toto.com', password: @user.password_digest)
+    assert_not user.valid?
+    assert_equal [:pseudo], user.errors.keys
+  end
+  
+  test 'ensures that the password length is minimumn 5' do
+    user = User.new(firstname: @user.firstname, lastname: @user.lastname, pseudo: 'titolino', email: 'tutu@toto.com', password: '1234')
+    assert_not user.valid?
+    assert_equal [:password], user.errors.keys
+  end
+  
 end
