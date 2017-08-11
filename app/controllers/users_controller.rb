96591e601a85aa93f18bd23d44b70106378a64f5
Sarah <sarah.composto@heig-vd.ch>
2017-08-11 19:30:27 +0000

    Add UsersController method to create an user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to tables_url, notice: 'Enregistrement effectué avec succès'
+        else
+            render :new
+        end
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:firstname, :lastname, :pseudo, :email, :password)
+    end
 end
