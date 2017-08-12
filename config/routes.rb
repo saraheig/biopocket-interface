44884b130972049f9f6893659634bab471a71041
Sarah <sarah.composto@heig-vd.ch>
2017-08-12 13:54:02 +0000

    Generate session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   resources :users, only: %i[new create]
+  resource :session, only: %i[new create destroy]
   get '/tables', to: 'tables#index'
   
   resources :works, path: '/tables/works'
