bff467eab8b3c8c4e43efa38ef42c8a4f54b70c3
Sarah <sarah.composto@heig-vd.ch>
2017-08-14 09:33:24 +0000

    Add Category search form

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -69,6 +69,6 @@ class TopicsController < AuthenticateController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def topic_params
-      params.require(:topic).permit(:title, :description, :search)
+      params.require(:topic).permit(:title, :description)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,12 @@ class Category < ApplicationRecord
   validates_length_of :title, :maximum => 50, :message => "Le titre du type d'action doit avoir moins de 50 caractères."
   validates_uniqueness_of :title, :case_sensitive => false, :message => "Le titre du type d'action est déjà utilisé."
   
+  def self.search(keyword)
+    if keyword
+      where('title iLIKE :term', term: "%#{keyword}%").order(updated_at: :desc)
+    else
+      all.order(updated_at: :desc)
+    end
+  end
+
 end
