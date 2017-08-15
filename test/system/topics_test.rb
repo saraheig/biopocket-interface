ad0d9e120267fab3d2bd5b8860a905afbf1bd38a
Sarah <sarah.composto@heig-vd.ch>
2017-08-15 14:03:24 +0000

    Add Topic system test

--- test/system/topics_test.rb
+++ test/system/topics_test.rb
@@ -1,9 +1,27 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class TopicsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit topics_url
-  #
-  #   assert_selector "h1", text: "Topic"
-  # end
+  setup do
+    visit new_session_url
+    assert_selector 'h1', text: 'Se connecter'
+
+    fill_in 'pseudo', with: 'YourPseudo'
+    fill_in 'password', with: 'yoursecret'
+
+    click_button 'Se connecter'
+    assert_selector '#notice', text: 'Connexion réussie'
+    assert_selector 'h1', text: 'Liste des tables'
+
+    @topic = topics(:one)
+  end
+
+  test 'visits the index' do
+    click_on 'Thèmes'
+    assert_selector 'h1', text: 'Table des thèmes'
+
+    within 'table' do
+      assert_selector 'td', text: @topic.title
+      assert_selector 'td', text: @topic.description
+    end
+  end
 end
