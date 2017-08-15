9d81d962f322d60c8a186f845d144f1cdf2134c1
Sarah <sarah.composto@heig-vd.ch>
2017-08-15 13:58:50 +0000

    Add Category system test

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -1,9 +1,26 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class CategoriesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit categories_url
-  #
-  #   assert_selector "h1", text: "Category"
-  # end
+  setup do
+    visit new_session_url
+    assert_selector 'h1', text: 'Se connecter'
+
+    fill_in 'pseudo', with: 'YourPseudo'
+    fill_in 'password', with: 'yoursecret'
+
+    click_button 'Se connecter'
+    assert_selector '#notice', text: 'Connexion réussie'
+    assert_selector 'h1', text: 'Liste des tables'
+
+    @category = categories(:one)
+  end
+
+  test 'visits the index' do
+    click_on 'Types d\'action'
+    assert_selector 'h1', text: 'Table des types d\'action'
+
+    within 'table' do
+      assert_selector 'td', text: @category.title
+    end
+  end
 end
