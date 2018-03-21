0dd6d6133dac4eef82bd2e8c93f6ac6060ca63fe
saraheig <sarah.composto@heig-vd.ch>
2018-03-21 18:17:30 +0000

    Check if a session is opened

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,4 @@
-class CategoriesController < ApplicationController
+class CategoriesController < AuthenticateController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
   
   # GET /categories
