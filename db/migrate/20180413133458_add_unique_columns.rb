e49b2e105c01e96621777c9a2ec8443aabf3d923
saraheig <sarah.composto@heig-vd.ch>
2018-04-27 10:09:16 +0000

    .

---
+++ db/migrate/20180413133458_add_unique_columns.rb
@@ -0,0 +1,14 @@
+class AddUniqueColumns < ActiveRecord::Migration[5.1]
+  def change
+    add_index :actions, :title, unique: true
+    add_index :actions_actions, [:ref_id, :dep_id], unique: true
+    add_index :categories, :title, unique: true
+    add_index :species, :name, unique: true
+    add_index :species_actions, [:action_id, :species_id], unique: true
+    add_index :tasks, :title, unique: true
+    add_index :tasks_tasks, [:ref_id, :dep_id], unique: true
+    add_index :themes, :title, unique: true
+    add_index :types, :title, unique: true
+    add_index :users, :pseudo, unique: true
+  end
+end
