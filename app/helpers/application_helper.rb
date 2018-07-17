7f643418d33a534c4b668a595ab89abd4b14b476
saraheig <sarah.composto@heig-vd.ch>
2018-07-17 10:07:26 +0000

    Modify parameters of markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   # Initialize a function related to markdown (parser)
   def markdown(text)
     if text
-      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, strikethrough: true)
       return markdown.render(text).html_safe
     end
   end
