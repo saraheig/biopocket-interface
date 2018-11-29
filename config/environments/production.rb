575e10791347c5b974f52e945fe871a0a8a6e5c5
saraheig <sarah.composto@heig-vd.ch>
2018-11-29 10:11:35 +0000

    Modify environment variable name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Base url for images
-  config.imagesBaseUrl = ENV["BIOPOCKET_IMAGES_BASE_URL"]
+  config.imagesBaseUrl = ENV["BIOPOCKET_INTERFACE_IMAGES_BASE_URL"]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
